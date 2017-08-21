4ddda0088cbeb391621bc9d4f113307dd96029ca
Bob Harper <bob.harper87@gmail.com>
2017-08-21 15:38:59 +0100

    needs a primary key

--- scraper.py
+++ scraper.py
@@ -1,22 +1,6 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
 # All that matters is that your final data is written to an SQLite database
@@ -29,15 +13,16 @@ from datetime import date
 
 def download_reg():
     if not os.path.exists(charity_reg_file):
-        urllib2.urlopen('http://www.charitycommissionni.org.uk/charity-search/?&exportCSV=1', charity_register) ## download today's Charity Register
+        urllib2.urlopen('http://www.charitycommissionni.org.uk/charity-search/?q=&include=Removed&exportCSV=1', charity_register) ## download today's Charity Register
 
 def get_charity_nums(charity_reg_file, charity_nums):
-    f = open(charity_reg_file, 'rU' ) #open the charity register in read universal mode
+    f = open(charity_reg_file, 'rU' )
     for line in f:
         cells = line.split( "," )
         charity_nums.append((cells[0]))
     f.close()
 
+    ## write each charity no. to the SqliteDB as PRIMARY KEYs
     for nic in charity_nums[1:]:
         try:
             c.execute("INSERT OR IGNORE INTO {tn} (id) VALUES ({nic})".\
@@ -55,7 +40,7 @@ def get_fieldnames():
         for row in reader:
             fieldnames.append(row['Field'])
             xpaths.append(row['xPath'])
-
+            ## write each field heading to the SqliteDB so we can write later
             try:
                 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}'"\
                     .format(tn=table_name, cn=row['Field']))
@@ -68,10 +53,8 @@ def get_fieldnames():
 
 def scrape_write_data(charity_num, page, pairings):
     tree = html.fromstring(page.content)
-    # go through each field in the reader and scrape data with xPath
-    # reader = csv.DictReader(fieldfile)
+    # go through each field in the xPath set and scrape data
     if page.status_code == requests.codes.ok:
-        newline = {}
         for field in pairings:
             fieldvalue = tree.xpath(field[1])
             newfieldvalue = []
@@ -82,9 +65,7 @@ def scrape_write_data(charity_num, page, pairings):
 
             c.execute("UPDATE {tn} SET {cn}=(:what) WHERE id=:nic".format(tn=table_name, cn=field[0]), {'what': item, "nic": int(charity_num)})
             conn.commit()
-
         print('updated entry for', charity_num)
-
     else:
         page.raise_for_status()
 
@@ -104,21 +85,20 @@ def query_charities(charity_nums, pairings):
             errors.append(ccnino)
             continue
 
-
 if __name__ == '__main__':
     url = "http://www.charitycommissionni.org.uk/charity-details"
     download_day = date.today().strftime('%Y%m%d')
     charity_reg_file = 'data/ccni' + download_day + '.csv'
     scraped_data = 'data/ccni_scraped' + download_day + '.csv'
-    sqlite_file = 'data.sqlite'
-    table_name = 'data'
+    sqlite_file = 'data.sqlite' ## morph.io requirement
+    table_name = 'data' ## morph.io requirement
     charity_nums = []
     errors = []
     pairings = get_fieldnames()
 
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS ' + table_name + '(id INTEGER)')
+    c.execute('CREATE TABLE IF NOT EXISTS ' + table_name + '(id INTEGER PRIMARY KEY)')
     conn.commit()
 
     download_reg()
@@ -126,13 +106,11 @@ if __name__ == '__main__':
     get_fieldnames()
     query_charities(charity_nums, pairings)
 
-    # conn, c = connect(sqlite_file)
-    # total_rows(c, table_name, print_out=True)
-    # table_col_info(c, table_name, print_out=True)
-    # values_in_col(c, table_name, print_out=True) # slow on large data bases
-
-    print('Data scraped from Charity Commission, except for these numbers:\n')
-    for no in errors:
-        print(no)
+    if len(errors) > 0:
+        print('Data scraped from Charity Commission, except for these numbers:\n')
+        for no in errors:
+            print(no)
+    else:
+        print('Data scraped from Charity Commission for',len(charity_nums),'entries')
 
     conn.close()
